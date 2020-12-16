ccc6d349af4d4f3c4131f8417ca9371ffa156d02
Eric Andrews <eric.andrews@streamr.com>
2020-12-17 01:05:40 +0200

    move shuffle to utils directory

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { Graph } from "./Graph";
 import { Event, TimeSimulator } from "./TimeSimulator";
-
-function shuffle<T>(arr: T[]): T[] {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-}
+import { shuffle } from "./utils";
 
 const graph = new Graph(10);
 
